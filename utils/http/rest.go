23de32b6ddd1b7039e4ec2bd434356497d4a4163
Jing Gao <jing.gao@outlook.com>
2021-05-17 19:02:10 +0800

    add SuccessRestResponse and FailedRestResponse

--- utils/http/rest.go
+++ utils/http/rest.go
@@ -7,3 +7,19 @@ type RestResponse struct {
 	Success bool        `json:"success"`
 	Data    interface{} `json:"data"`
 }
+
+func SuccessRestResponse() *RestResponse {
+	return &RestResponse{
+		Code:    200,
+		Message: "sucess",
+		Success: true,
+	}
+}
+
+func FailedRestResponse() *RestResponse {
+	return &RestResponse{
+		Code:    200,
+		Message: "failed",
+		Success: false,
+	}
+}
